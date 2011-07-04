c3f8a6d65c126975cd999145ea6fe2c94aa02189
Dojo Online <dojo.online.oficial@gmail.com>
2011-07-03 23:20:55 -0400

    arquivo do dojo do dia 26 de junho de 2011

---
+++ 26_06_2011/parenteses.py
@@ -0,0 +1,18 @@
+import string
+
+def parentese(entrada):
+#	if entrada == 'True and True or False':
+#		return 2
+#	return 1
+
+	count = 0
+
+	for i in range(10):
+		if i % 2 == 0:
+				
+
+	if(eval(entrada)):
+		count = count+1
+	
+	return count
+
